6caed41192000431d43eb1784c4ddc70038294b3
root <root@localhost.localdomain>
2019-12-08 17:01:58 +0900

    Added login form

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
-<?php 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="ko" xml:lang="ko" xmlns="http://www.w3.org/1999/xhtml">
 
-echo "hello world!";
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title></title>
+	<meta name="robots" content="noindex, nofollow" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta name="author" content="" />
+    <link rel="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/css/uikit.min.css"/>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.2.4/dist/css/uikit.min.css" />
 
-?>
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.4/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.4/dist/js/uikit-icons.min.js"></script>
+</head>
+<body>
+
+    <div class="uk-section uk-container">
+        <div class="uk-grid uk-child-width-1-3@s">
+            <form class="">
+                <div class="">
+                    <label class="" for="form-stacekd-text">Email</label>
+                    <div class="">
+                        <input class="" id="form-stacked-text" type="email" required="" />
+                    </div>
+                </div>
+                <div class="">
+                    <label class="" for="form-stacekd-text">Password</label>
+                    <div class="">
+                        <input class="" id="form-stacked-text" type="password" required="" />
+                    </div>
+                </div>
+                <div class="">
+                    <button class="" type="submit">Login</button>
+                </div>
+            
+            </form>        
+        </div>
+    
+    
+    </div>
+
+</body>
+</html>
